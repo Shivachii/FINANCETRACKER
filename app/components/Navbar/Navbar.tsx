761b4dcdeb7cfd8207bf2bd5279be1471c1ae2db
Shivachii <videllkonya@gmail.com>
2025-03-09 19:38:33 +0300

    navbar fix

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,17 +1,17 @@
 "use client";
-import { useState } from "react";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { SmallLogo } from "../Logo/Logo";
-import { Menu } from "lucide-react"; // Add menu icon
 import {
   ChartColumnBig,
   HomeIcon,
+  Menu,
   Settings,
   TrendingDown,
   TrendingUp,
 } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const navLinks = [
   { name: "Dashboard", href: "/", logo: HomeIcon },
@@ -23,18 +23,67 @@ const navLinks = [
 
 export default function Navbar() {
   const pathname = usePathname();
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      {/* Mobile Menu Button */}
+      <div className="md:hidden">
+        <SmallNavbar />
+      </div>
+      <div className="hidden h-screen w-64 bg-slate-50 p-4 md:flex flex-col sticky top-0 left-0">
+        <div className="mb-8 flex gap-8 items-center ">
+          <SmallLogo />
+        </div>
+        <nav className="flex flex-col flex-grow gap-2">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.href} key={link.name} className="block">
+                <span
+                  className={`${
+                    pathname === link.href
+                      ? "bg-gray-200 font-semibold border-l-blue-500 border-4 border-r-0 border-y-0"
+                      : "text-black"
+                  } py-2 px-4 rounded-l-none rounded-md hover:bg-slate-200 flex items-center gap-4`}
+                >
+                  {link.logo && <link.logo />} {link.name}
+                </span>
+              </Link>
+            );
+          })}
+        </nav>
+        <div className="flex  my-10 items-center gap-2 py-2 px-4">
+          <span className="text-gray-600 text-sm font-bold">
+            {" "}
+            Logged in as :
+          </span>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SmallNavbar() {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => setIsDesktop(window.innerWidth >= 768);
+    checkScreenSize(); // Check on mount
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+  return (
+    <>
+      {/* MOBILE MENU BUTTON */}
       <button
-        className="md:hidden p-2 fixed top-4 right-0 z-50 bg-gray-100 rounded-md shadow-md"
+        className="md:hidden   p-2 fixed top-4 right-2 z-50 bg-gray-100 rounded-md shadow-md"
         onClick={() => setIsOpen(!isOpen)}
       >
         <Menu size={24} />
       </button>
-
       {/* Sidebar */}
       <div
         className={`h-screen w-56 md:w-64 bg-slate-50 p-4 flex flex-col fixed top-0 left-0 transition-transform duration-300 z-40 ${
@@ -45,7 +94,6 @@ export default function Navbar() {
         <div className="mb-8 flex gap-8 items-center">
           <SmallLogo />
         </div>
-
         {/* Navigation Links */}
         <nav className="flex flex-col flex-grow gap-2">
           {navLinks.map((link) => (
@@ -62,13 +110,11 @@ export default function Navbar() {
                     : "text-black"
                 }`}
               >
-                <link.logo size={20} />{" "}
-                {isOpen || window.innerWidth >= 768 ? link.name : ""}
+                <link.logo size={20} /> {isOpen || isDesktop ? link.name : ""}
               </span>
             </Link>
           ))}
         </nav>
-
         {/* User Section */}
         <div className="flex my-10 items-center gap-2 py-2 px-4">
           <span className="text-gray-600 text-sm font-bold">Logged in as:</span>
